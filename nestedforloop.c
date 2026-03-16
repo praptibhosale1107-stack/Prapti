42dfd6ffd4b2142dc33d2ac75fd3bcf1aa85df4a
praptibhosale1107-stack <praptibhosale1107@gmail.com>
2026-03-17 00:06:33 +0530

    Add files via upload

---
+++ nestedforloop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+    int i,j;
+    for(i=1;i<=3;i++)
+    {
+        printf("A");
+        for(j=i;j<=4;j++)
+       {
+        printf("B");
+       }
+    }
+        printf("C");  
+} 
