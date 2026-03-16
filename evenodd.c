9d2a26a9af86c0f252dd156ad64a425f21a02665
praptibhosale1107-stack <praptibhosale1107@gmail.com>
2026-03-17 00:25:07 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int x;
+  printf("enter a number: ");
+  scanf("%d", &x);
+  if (x > 0)
+  {
+    printf("this is even");
+  }
+  else
+  {
+    printf("this is odd");
+  }
+0}
